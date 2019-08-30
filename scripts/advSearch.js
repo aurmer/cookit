1effaef749e12630b725cf1390a93e2bb4041cc0
Taylor Blocker <taylorblocker@gmail.com>
2019-08-29 21:21:27 -0500

    created files for working on advanced search bar

---
+++ scripts/advSearch.js
@@ -0,0 +1,3 @@
+//Creating advanced search feature here
+//Would like to create a list of parameters based off information in edeman-data.js
+//Then need to toggle them on and off to add or remove search parameters
