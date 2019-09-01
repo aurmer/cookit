2c21fa16d73eb719bb69c2cc5d80a1023b0932a3
Taylor Blocker <taylorblocker@gmail.com>
2019-09-01 09:56:52 -0500

    Craeted html for general checkboxes. Now need to add them to search parameters.

--- scripts/advSearch.js
+++ scripts/advSearch.js
@@ -1,3 +1,9 @@
 //Creating advanced search feature here
 //Would like to create a list of parameters based off information in edeman-data.js
 //Then need to toggle them on and off to add or remove search parameters
+
+document.addEventListener('DOMContentLoaded',function()
+{
+    
+
+});
